d6e25b392db268dbc3685b2718e192214a645661
Matthew Malone <mmalone@composableanalytics.com>
2025-06-13 12:07:55 -0400

    First

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab_ca_theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_ca_theme:plugin',
+  description: 'A JupyterLab extension theme for Composable Analytics DataLabs.',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab_ca_theme is activated!');
+    const style = 'jupyterlab_ca_theme/index.css';
+
+    manager.register({
+      name: 'jupyterlab_ca_theme',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
